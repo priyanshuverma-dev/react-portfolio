94c5c778d122ab7f64f6427e75d3c42e6bf33b93
codebyps <antrikshdev@gmail.com>
2023-06-19 19:01:10 +0530

    👌 bind all

---
+++ src/data/WorkData.js
@@ -0,0 +1,65 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "Gita Mobile",
+    description:
+      "Mobile application allows you to read and understand power og Bhagavd Gita. ",
+    tags: ["Flutter", "Getx", "fMongoDB"],
+
+    demo: "https://github.com/codebyps/GITAMOBILE/releases",
+    github: "https://github.com/codebyps/GITAMOBILE",
+  },
+  {
+    id: 2,
+    name: "Tik Tok Clone",
+    description:
+      "Web application works similar to Tik Tok. Users can create and share their own videos. Users can also like and comment on other users' videos.",
+    tags: ["react", "tailwind", "sanity"],
+
+    demo: "https://clone-tok.vercel.app/",
+    github: "https://github.com/codebyps/clone_tik",
+  },
+  {
+    id: 3,
+    name: "NetFlix Clone",
+    description:
+      "Web application works similar to NetFlix. Users can stream videos.",
+    tags: ["Nextjs", "styledComponents", "MongoDB"],
+
+    demo: "https://priflex.vercel.app",
+    github: "https://github.com/codebyps/priflex",
+  },
+  {
+    id: 4,
+    name: "AI Chat",
+    description:
+      "Web-based platform that allows users to search, filter, and view solutions to common problems.",
+    tags: ["Vanilla", "ChatGPT4", "tailwind"],
+
+    demo: "https://somveers.me/aichat/",
+    github: "https://github.com/codebyps/aichat",
+  },
+  {
+    id: 5,
+    name: "Delta",
+    description:
+      "Mobile application that enables users to search for answers to common questions and share their own answers.",
+    tags: ["flutter", "ChatGPT"],
+
+    demo: "https://github.com/codebyps/delta",
+    github: "https://github.com/codebyps/delta",
+  },
+
+  //   {
+  //     id: 6,
+  //     name: "Responsive Navigation Menu",
+  //     description:
+  //       "A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+  //     tags: ["react", "react-router", "css"],
+
+  //     demo: "http://react-responsive-navbar-codebucks27.vercel.app/",
+  //     github: "https://github.com/codebucks27/React-responsive-navbar",
+  //   },
+];
